c01f7e97d5a645375ece21e0f7397d1f2621be36
Guy haiby <ghaiby@ucsc.edu>
2024-12-03 19:51:42 -0800

    formating main.ts

--- src/main.ts
+++ src/main.ts
@@ -302,7 +302,7 @@ function growPlants(): void {
         const hasPlayer = cell.content.includes("P");
         const cellContentNoPlayer = cell.content.replace("P", "");
 
-        const cellContent = growthStages[cellContentNoPlayer] ||
+        const cellContent = growthStages[cellContentNoPlayer] || 
         cellContentNoPlayer;
         cell.content = hasPlayer ? cellContent + "P" : cellContent;
         cell.water--; // reduce cell water by two for growing plant
